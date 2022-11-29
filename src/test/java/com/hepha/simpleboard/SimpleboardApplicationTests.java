8ec45b55464aaddea30416a749b92fce12e851fe
unknown <180012@hjskr.hanjin.com>
2022-11-29 17:08:45 +0900

    test

---
+++ src/test/java/com/hepha/simpleboard/SimpleboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hepha.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
