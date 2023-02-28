d8659059ccdd01db52e9021c917ccb2b97a32a4e
YONGHAPAPA <zerobreaker@gmail.com>
2023-02-28 19:05:53 +0900

    dev post

--- src/main/java/com/hepha/simpleboard/controller/BoardController.java
+++ src/main/java/com/hepha/simpleboard/controller/BoardController.java
@@ -3,7 +3,6 @@ package com.hepha.simpleboard.controller;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,41 +13,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.hepha.simpleboard.model.Article;
+import com.hepha.simpleboard.model.Post;
 import com.hepha.simpleboard.model.Board;
-import com.hepha.simpleboard.model.Book;
-import com.hepha.simpleboard.repository.ArticleRepository;
-import com.hepha.simpleboard.repository.BookRepository;
-import com.hepha.simpleboard.service.ArticleService;
+import com.hepha.simpleboard.repository.PostRepository;
+import com.hepha.simpleboard.service.PostService;
 import com.hepha.simpleboard.service.BoardService;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@RestController
+// [M.RMK]2023.02.28 
+// @Controller와는 다르게 @RestController는 리턴값에 자동으로 @ResponseBody 를 붙임 
+@RestController 
+@RequestMapping("/board")
 //@Controller
 public class BoardController {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    @Qualifier("namedParameterJdbcBookRepository")    
-    private BookRepository bookRepository;
+    //@Autowired
+    //@Qualifier("namedParameterJdbcBookRepository")    
+    //private BookRepository bookRepository;
+
+    // @Autowired
+    // private PostRepository postRepository;
 
     @Autowired
-    private ArticleRepository articleRepository;
+    private PostService postService;
 
     @Autowired
-    private ArticleService articleService;
-
-    @Autowired
-    private BoardService brdSvc;
+    private BoardService boardService;
 
 
     private final Logger logger = LogManager.getLogger(BoardController.class);
@@ -57,60 +58,24 @@ public class BoardController {
 
 
     
-    //@ResponseStatus(code=HttpStatus.NOT_FOUND, reason="Data Not Found") //강제 Exception 발생 처리 
-    @RequestMapping(value="board/getAllBoardList", method=RequestMethod.GET)
-    //@GetMapping("/board/getBoardList")
-    @ResponseBody
-    public Object getBoardList(){
-
+    //[M.CHK]2023.02.28 컨트롤러에 해당 경로의 매핑 메서드가 없는경우 없는 경우 front 에서 어떤방식으로 exception 처리방법    
+    //[M.RMK]2023.02.28 GetMapping 은 메서드에만 적용, HTTP GET 요청을 핸들러 메서드에 매핑하기 위함.
+    //@ResponseStatus(code=HttpStatus.NOT_FOUND, reason="Data Not Found") //강제 Exception 발생 처리     
+    @GetMapping("/getAllBoards")    
+    public List<Board> getAllBoards(){
         //logger.debug("request to >>>>> getBoardList");
-
-        List<Board> result = brdSvc.getBoardList();
-
+        List<Board> result = boardService.getBoardList();
         //logger.debug(result);
-
-        for(int i = 0; i< result.size(); i++){
-            Board brd = result.get(i);
-            //logger.debug(brd.getBoardName());
-            logger.debug(brd.getBoardName());
-        }
-
-
-        
-
-
-        
-
         return result;
-
-        
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    @GetMapping("/board/hello")
-    public List<String> hello(){
-
-        logger.debug("hello....2");
-        return Arrays.asList("hi1", "hello1");
-    }
-
-    @GetMapping("/board/articlelist")
-    public List<Article> articleList(){
+    //2023.02.26 getPostsByBoardId 구현하기
+    @GetMapping("/getPostsByBoardIdBoard")
+    public List<Post> getPostsByBoardIdBoard(@RequestParam String boardId){
         logger.debug("Standard :: Article List >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 2222");
 
         // ModelAndView model = new ModelAndView();
-         List<Article> result = articleService.getArticleList();
+         List<Post> result = postService.getArticleList();
 
 
         //List<Article> result = articleRepository.findAll();
@@ -141,8 +106,6 @@ public class BoardController {
             new Book("3", "cccc_", "title_cccc")
         );
 
-
-
         books.forEach(book -> {
             logger.debug(book.getId() + ":" + book.getName());
             //bookRepository.save(book);
@@ -152,28 +115,14 @@ public class BoardController {
         });
         */
 
-
         //조회 개수 가져오기
         // int count = articleRepository.getCountByArticleType("C");
         // logger.debug("article_count 2 : " + count);
-
 
         //Article 생성하기
         // Article newArticle = new Article("1", "this is title....", "this is contentsssss", "N", "N", "mztest", new Date(), "mztest", new Date());
         // int newCnt = articleRepository.create(newArticle);
         // logger.debug("newCnt >>> " + newCnt);
-
-
-        //Article 리스트 가져오기
-        List<Article> article_list = articleRepository.findAll();
-
-        logger.debug("article_list.size: " + article_list.size());
-
-        //for(int i=0; i < article_list.size(); i++)
-        for(Article item : article_list){
-            logger.debug("ID : " + item.toString());
-        }
-
         return model;
     }
 
